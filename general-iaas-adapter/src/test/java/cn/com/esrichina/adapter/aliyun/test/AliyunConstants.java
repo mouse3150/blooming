52330607bd52da227e732a693058485f30b2c2bb
chenhao <mouse3150@163.com>
2020-09-24 15:42:38 +0800

    Update AliyunConstants.java
    删除access_key

--- general-iaas-adapter/src/test/java/cn/com/esrichina/adapter/aliyun/test/AliyunConstants.java
+++ general-iaas-adapter/src/test/java/cn/com/esrichina/adapter/aliyun/test/AliyunConstants.java
@@ -3,7 +3,7 @@ package cn.com.esrichina.adapter.aliyun.test;
 public class AliyunConstants {
 	public static final String PLATFORM = "aliyun";
 	public static final String SERVER_URL = "https://ecs.aliyuncs.com/";
-	public static final String ACCESS_KEY_ID = "EoBrcYwb0E2tP37T";
+	public static final String ACCESS_KEY_ID = "******";
 	public static final String ACCESS_KEY_SECRET = "zl1GHwD77nWtorfUWyDpjLQ7hdpDS0";
 	
 	public static final String DB_INSTANCE_ID = "rdsajvqmqajvqmq";
